5c82c1a4f0b21245d987f749ac189f6dbb9166d3
Matthew Russell <mrussell5361@gmail.com>
2022-04-14 16:07:54 -0400

    Added Get and Post method

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -28,5 +28,27 @@ namespace RestaurantRaterApi.Controllers
             
             return View(restaurants);
         }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(RestaurantCreate model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.errorMessage = ModelState.FirstOrDefault().Value.Errors.FirstOrDefault().ErrorMessage;
+                return View(model);
+            }
+            Restaurant restaurant = new Restaurant()
+            {
+                Name = model.Name,
+                Location = model.Location,
+            };
+            _context.Restaurants.Add(restaurant);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
